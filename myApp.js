e2f41cea7bfff4368abd79345b04bf1f063b6fc9
azario0 <azariomalek@gmail.com>
2023-03-31 01:14:06 +0100

    added few changes

--- myApp.js
+++ myApp.js
@@ -1,39 +1,28 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+// Adding the public css for the whole page
+app.use("/public", express.static(__dirname + "/public"));
 
+let respponse = "";
+console.log("Hello World!");
 
+// responding with File
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+// Responding with json
+app.get("/json", (req, res) => {
+  //This is to work with the .env file .
+  require("dotenv").config();
 
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = "Hello json".toUpperCase();
+  } else {
+    response = "Hello json";
+  }
+  res.json({ message: response });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
